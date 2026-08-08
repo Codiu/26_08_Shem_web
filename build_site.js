35d6e4a84f089628689d3a9dfa012095323e7d0d
Евгенич <26188987+Codiu@users.noreply.github.com>
2026-08-08 16:45:57 +0300

    fix : image fit to screen

--- build_site.js
+++ build_site.js
@@ -67,7 +67,7 @@ pages.forEach(p => {
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Outfit:wght@400;500;600;700&family=Inter:wght@300;400;500;600;700&display=swap" rel="stylesheet">
-  <link rel="stylesheet" href="style.css?v=11">
+  <link rel="stylesheet" href="style.css?v=13">
 </head>
 <body class="pearl-theme">
 
@@ -96,10 +96,10 @@ ${content}
 
 ${footerHtml}
 
-  <script src="cart.js?v=11"></script>
-  <script src="shop.js?v=11"></script>
-  <script src="cart-page.js?v=11"></script>
-  <script src="script.js?v=11"></script>
+  <script src="cart.js?v=13"></script>
+  <script src="shop.js?v=13"></script>
+  <script src="cart-page.js?v=13"></script>
+  <script src="script.js?v=13"></script>
 </body>
 </html>
 `;
